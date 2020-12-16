37fcaf269043a28c3dda5fb76b3ba5c089df537a
Aditya Chaturvedi <chat29aditya@gmail.com>
2020-12-16 18:28:41 +0530

    Revert "WIP"
    This reverts commit a43df05b827613ff7d5aebcad27393f3325858f2.

--- restApi/models.py
+++ restApi/models.py
@@ -57,9 +57,6 @@ class User(AbstractBaseUser, models.Model):
     def is_staff(self):
         self.isAdmin
 
-class TopicManager(models.Manager): 
-    def topicNames(self): 
-        return self.filter(id__lt = 5)
 
 class Topic(models.Model):
     id = models.AutoField(primary_key=True)
@@ -67,8 +64,6 @@ class Topic(models.Model):
     shortDesc = models.CharField(max_length=40)
     insertedAt = models.DateTimeField(auto_now=True)
     updatedAt = models.DateTimeField(auto_now=True)
-
-    objects = TopicManager()
 
 class UserTopicRelationship(models.Model): 
     id = models.AutoField(primary_key=True)
@@ -112,6 +107,7 @@ class TopicEventRelationship(models.Model):
 
     class Meta: 
         unique_together= ("topic", "event")
+
 
 # class UserAccount(AbstractBaseUser):
 #     email = models.EmailField(unique=True)
